90f4f2d7bf18b1fc8a0917e918eab255414f86af
James Mighty <jhuda88@gmail.com>
2020-03-06 21:58:56 +0100

    little edit

--- Example1/Example1/Example1.py
+++ Example1/Example1/Example1.py
@@ -2,12 +2,21 @@ import datetime
 import time
 
 three = [
-    (["ahoj","cus"],lambda: print("nazdar")),
-    (["hodin"], lambda: print(str(datetime.date.today())))
+    (["ahoj","cus"],
+     lambda: print("nazdar")),
+    (["se mas","ti je"],
+     lambda: print("celkem fajn")),
+    (["datum"],
+     lambda: print(datetime.datetime.now().strftime("%Y/%m/%d"))),
+    (["cas","hodin"],
+     lambda: print(datetime.datetime.now().strftime("%H:%M:%S"))),
     ]
 
 while True:
     inp = input()
-    for tuple in three:
-        if any(condition in inp for condition in tuple[0]):
+    for tuple in three: 
+        if any(condition in inp.lower() for condition in tuple[0]):
             tuple[1]()
+
+
+# Zkus do konzole napsat neco jako: Ahoj, jak se mas?
