cd1139dd1b0ed16fd99a3cd9e5c7f49a9ede8742
James Mighty <jhuda88@gmail.com>
2020-03-08 00:05:48 +0100

    refactor - better recursion

--- Example1/Example1/Example2.py
+++ Example1/Example1/Example2.py
@@ -3,6 +3,14 @@ import time
 import unicodedata
 import re
 
+def any_common(a, b): 
+    a_set = set(a) 
+    b_set = set(b) 
+    if (a_set & b_set): 
+        return True 
+    else: 
+        return False
+
 def strip_accents(s):
    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
 
@@ -10,7 +18,9 @@ three = [
     (["ahoj","cus"],
      lambda inp: print("nazdar"),
      [
-         (["se mas", "ti je"], lambda inp: print("celkem fajn"))
+         (["se mas", "ti je"],
+          lambda inp: print("celkem fajn")
+         )
      ]
     ),
     (["se mas","ti je"],
@@ -19,37 +29,44 @@ three = [
     (["datum"],
      lambda inp: print(datetime.datetime.now().strftime("%Y/%m/%d"))
     ),
-    (["cas","hodin"],
+    (["cas","hodin", "kolik je"],
      lambda inp: print(datetime.datetime.now().strftime("%H:%M:%S"))
     ),
-    (["chapes?","neopic"],
-     lambda inp: print(inp)
+    (["neopic"],
+     lambda inp: print(inp),
+     [
+         (["prosim"],
+            lambda inp: [print("ok, sorry"),inp][-0]
+         )
+     ]
     ),
     (["s{3}"],
      lambda inp: print("nope")
     )
     ]
 
-def Resolve(three, inp=None):
+def Resolve(three, inp=None, allResolvedStrings=[]):
     doPrintUnresolved = True
     resolved = False
     if inp == None:
-        inp = strip_accents(input().lower())
+        inp = strip_accents(input("> ").lower())
     else:
         doPrintUnresolved = False
     for tuple in three: 
-        if any((string in inp or re.match(string,inp)) for string in tuple[0]):
-            tuple[1](inp)
+        matchingStrings = [string for string in tuple[0] if (string in inp or re.match(string,inp))]
+        if len(matchingStrings) > 0 and not any_common(matchingStrings,allResolvedStrings):
+            result = tuple[1](inp)
             resolved = True
+            allResolvedStrings+=matchingStrings
             if len(tuple) == 3:
-                if Resolve(tuple[2],inp):    
-                    break   
+                Resolve(tuple[2],inp,allResolvedStrings)
     if doPrintUnresolved and not resolved:
         print("co?")
+    #print(allResolvedStrings)
     return resolved
 
 while True:
-    Resolve(three)
+    Resolve(three, None, [])
 
 
 # Zkus do konzole napsat neco jako: Ahoj, jak se mas? nebo Ahoj, kolik je hodin?
