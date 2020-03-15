e85f9b1e2d635b0110324d3096e2dce0de89288c
James Mighty <jhuda88@gmail.com>
2020-03-15 19:50:15 +0100

    word order

--- program.py
+++ program.py
@@ -3,11 +3,11 @@ import resolver
 
 while True:
     inp = input("> ")
-    todo = resolver.Resolve(Three, inp=inp, allResolvedStrings=[])[0]
+    todo = resolver.Resolve(Three, inp=inp, allResolvedMatches=[], lastMatch=0)[0]
     #print(todo)
     outp = resolver.Do(todo,inp)
     print(outp)
     
 
 
-# Zkus do konzole napsat neco jako: Ahoj, jak se mas? nebo Ahoj, kolik je hodin?
+# Zkus do konzole napsat neco jako: Ahoj, jak se mas? nebo Ahoj, kolik je hodin?allResolvedMatchesallResolvedMatchesallResolvedMatchesallResolvedMatchesallResolvedMatches
--- resolver.py
+++ resolver.py
@@ -3,15 +3,15 @@ import re
 
 def Do(todoList, inp):
     todoList = sorted(todoList, key = lambda item: item[0])
-    wholeOutput = "" # sberac
+    wholeOutput = ""
     for item in todoList:
-        outp = item[1].OwnCommand(inp) # vlastni vystup
-        wholeOutput += (outp if outp is not None else "" ) + " "
+        outp = item[1].OwnCommand(inp)
+        wholeOutput += (outp if outp is not None else "" ) + ". "
         if len(item[2]) > 0:
             wholeOutput += Do(item[2],inp)
     return wholeOutput
 
-def Resolve(three, inp=None, allResolvedStrings=[]):
+def Resolve(three, inp=None, allResolvedMatches=[], lastMatch=0):
     doPrintUnresolved = True
     resolved = False
     todo = []
@@ -21,18 +21,20 @@ def Resolve(three, inp=None, allResolvedStrings=[]):
         inp = strip_accents(inp.lower())
         doPrintUnresolved = False
     for comm in three: 
-        matchingStrings = [string for string in comm.Conditions if string in inp] + [string for string in comm.Conditions if re.search(string,inp) is not None]
-        indexes = [inp.index(string) for string in comm.Conditions if string in inp ] + [re.search(string, inp).span()[0] for string in comm.Conditions  if re.search(string,inp) is not None]
-        if len(matchingStrings) > 0 and not any_common(matchingStrings,allResolvedStrings):
-            inp = comm.Decorator(inp) # mozna se bude nekdy hodit, idk
-            resolved = True
-            allResolvedStrings+=matchingStrings
-            if len(comm.CommandList) > 0:
-                todo.append( (min(indexes),comm, Resolve(comm.CommandList,inp,allResolvedStrings)[0]) )
-            else:
-                todo.append( (min(indexes),comm, []) )
+        matches = [(inp.index(string),string) for string in comm.Conditions if string in inp] + [(re.search(string, inp).span()[0], string) for string in comm.Conditions if re.search(string,inp) is not None]
+        if len(matches) > 0:
+            matchOn = min([match[0] for match in matches])
+            if not any_common([match[1] for match in matches],[resolvedMatch[1] for resolvedMatch in allResolvedMatches]) and lastMatch <= matchOn:
+                matches = sorted(matches, key=lambda match: match[0])
+                inp = comm.Decorator(inp) # mozna se bude nekdy hodit, idk
+                resolved = True
+                allResolvedMatches+=matches
+                if len(comm.CommandList) > 0:
+                    additionalTodo =  Resolve(comm.CommandList,inp,allResolvedMatches, lastMatch=matchOn)[0]
+                    todo.append( (matchOn,comm, additionalTodo) )
+                else:
+                    todo.append( (matchOn,comm, []) )
             
     if doPrintUnresolved and not resolved:
         print("co?")
-    #print(allResolvedStrings)
     return todo,inp
