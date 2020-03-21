5f5380cefd275dca65a08fe4800a39d3af3b4c5a
JamesMighty-AAPC <jhuda88@gmail.com>
2020-03-21 19:46:13 +0100

    created some basic syntax check test

--- gauge-tests/steps/step_impl.py
+++ gauge-tests/steps/step_impl.py
@@ -1,17 +1,31 @@
-from getgauge.python import step, before_scenario, Messages
+from getgauge.python import step, before_scenario, Messages, DataStore
 import resolver
 import nodeThree
 from command import *
 
-@step("The node <node> has ahead syntax with argument node <arg>")
-def node_has_ahead_syntax(node, arg):
+
+@step("The node <node> has syntax <flagName> with argument node <arg>")
+def node_has_specified_syntax_flag(node,flagName, arg):
+    flag = SyntaxE.from_str(flagName)
+    print("Checking "+flagName+" syntax for "+node)
+
+    print("     Ahead syntax check")
     inp = node+" "+arg
     todo, inp2 = resolver.Resolve(nodeThree.Three, inp=inp,allResolvedMatches=[])
     print(todo)
-    assert SyntaxE.Ahead in todo[0][1].Syntax, "node does not have SyntaxE.Ahead set"
-    assert len(todo[0][2]) > 0, "node does not have any sub-todolist"
-    outp = resolver.Do(todo,inp)
-    return
+    if SyntaxE.Ahead in flag or SyntaxE.Slack in flag:
+        assert len(todo[0][2]) > 0, "node does not have any sub-todolist when node before args"
+    if SyntaxE.Next in flag:
+        assert len(todo[0][2]) == 0, "node does have sub-todolist when node is after args"
+
+    print("     Next syntax check")
+    inp = arg+" "+node
+    todo, inp2 = resolver.Resolve(nodeThree.Three, inp=inp,allResolvedMatches=[])
+    print(todo)
+    if SyntaxE.Next in flag or SyntaxE.Slack in flag:
+        assert len(todo[0][2]) > 0, "node does not have sub-todolist when node is after args"
+    if SyntaxE.Ahead in flag:
+        assert len(todo[0][2]) == 0, "node does have sub-todolist when node is after args"
 
 @before_scenario()
 def before_scenario_hook():
