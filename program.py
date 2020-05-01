3d598e1c7a62f7e65f85f05906b574d3f38c2aac
pavel-marincak <61801828+pavel-marincak@users.noreply.github.com>
2020-05-01 09:59:39 +0200

    Kosmetické upravy

--- program.py
+++ program.py
@@ -5,12 +5,15 @@ from data.utility import TTS
 import pygame
 
 pygame.mixer.init()
+
+name = input("Zadej své jméno: ") # Pak to vysperkujeme na users nebo tak slo mi jen o vzhled
+
 while True:
-    inp = input("> ")
-    todo = resolver.Resolve(Three, inp=inp, allResolvedMatches=[], lastMatch=0, doPrintUnresolved=True)[0]
+    inp = input(name + "-> ") # co se tice toho znaku -> nic lepsiho me nenapadlo ale mužes si vybrat  May:>, May>>>, May:, atd. 
+    todo = resolver.Resolve     (Three, inp=inp, allResolvedMatches=[], lastMatch=0, doPrintUnresolved=True)[0]
     #print(todo)
     outp = resolver.Do(todo,inp)
-    print(outp)
+    print("May-> " + outp)
     TTS(outp)
 
 
